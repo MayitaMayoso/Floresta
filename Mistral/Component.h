e6df54210c6c755a52823a2097fd67e93cc634a1
Agustín Molina Uasuf <Molina.uasuf@gmail.com>
2024-09-19 14:48:09 +0200

    Components, resources and Input managers

---
+++ Mistral/Component.h
@@ -0,0 +1,41 @@
+#ifndef FLORESTAPROJECT_COMPONENT_H
+#define FLORESTAPROJECT_COMPONENT_H
+
+#include <string>
+#include <vector>
+
+#include "raylib-cpp.hpp"
+
+class Component
+{
+  public:
+
+	// Getters
+	std::string GetId();
+
+	Component* GetParent();
+
+	std::vector<Component*>& GetChildren();
+
+	// Events
+	virtual void CreateEvent() {};
+
+	virtual void DestroyEvent() {};
+
+	virtual void UpdateEvent() {};
+
+	virtual void FixedUpdateEvent() {};
+
+	virtual void RenderEvent() {};
+
+	virtual void RenderScreenEvent() {};
+
+  private:
+
+	std::string mId;
+	Component* mParent;
+	std::vector<Component*> mChildren;
+	raylib::Vector3 mPosition;
+};
+
+#endif // FLORESTAPROJECT_COMPONENT_H
